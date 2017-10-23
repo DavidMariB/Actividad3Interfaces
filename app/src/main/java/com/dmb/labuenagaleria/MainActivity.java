109a4f53fdef3a76be2e63f578e267fc54cac2c1
jomabe <dmari.scfe11@gmail.com>
2017-10-23 17:18:51 +0200

    Añadidos xml para hacer los marcos de los textview

--- app/src/main/java/com/dmb/labuenagaleria/MainActivity.java
+++ app/src/main/java/com/dmb/labuenagaleria/MainActivity.java
@@ -2,8 +2,14 @@ package com.dmb.labuenagaleria;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
